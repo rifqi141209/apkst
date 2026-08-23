aa654dcb5569fb2ab1cd25c2410dab892ab96057
rifqi141209 <muhamadrifqis234@gmail.com>
2026-08-23 11:17:12 +0700

    refactor database configuration to prioritize MySQL environment variables

--- config/database.php
+++ config/database.php
@@ -34,7 +34,7 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'url' => env('DB_URL'),
+            'url' => env('MYSQL_URL', env('DB_URL', env('DATABASE_URL'))),
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
@@ -47,11 +47,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', env('MYSQLHOST', '127.0.0.1')),
-            'port' => env('DB_PORT', env('MYSQLPORT', '3306')),
-            'database' => env('DB_DATABASE', env('MYSQLDATABASE', 'laravel')),
-            'username' => env('DB_USERNAME', env('MYSQLUSER', 'root')),
-            'password' => env('DB_PASSWORD', env('MYSQLPASSWORD', '')),
+            'host' => env('MYSQLHOST', env('DB_HOST', '127.0.0.1')),
+            'port' => env('MYSQLPORT', env('DB_PORT', '3306')),
+            'database' => env('MYSQLDATABASE', env('DB_DATABASE', 'laravel')),
+            'username' => env('MYSQLUSER', env('DB_USERNAME', 'root')),
+            'password' => env('MYSQLPASSWORD', env('DB_PASSWORD', '')),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
